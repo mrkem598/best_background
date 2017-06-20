58eb982eb8b1300e28fc4fcfb70ef70d7b0fd50c
Moh <mohakemal9@gmail.com>
2017-06-19 23:25:05 -0400

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -124,4 +124,115 @@ function resetGame() {
 	generateHTML();
 	timerWrapper();
 }
+var canvas = document.createElement("canvas")
+document.getElementsByTagName("body")[0].appendChild(canvas)
+var ctx = canvas.getContext("2d")
+
+var bg = [255,255, 50]
+var num = 19
+
+function setup() {
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+  ctx.translate(canvas.width / 2, canvas.height / 2)
+  fill()
+ 
+  
+  sharedLoop1 = new Loop(180 + Math.floor(Math.random() * 380))
+  sharedLoop2 = new Loop(180 + Math.floor(Math.random() * 380))
+}
+
+function fill(amt) {
+    ctx.globalCompositeOperation = 'screen';
+  ctx.beginPath();
+  ctx.rect(- canvas.width / 2, - canvas.height / 2, canvas.width, canvas.height)
+  ctx.fillStyle = `rgba(${bg[0]}, ${bg[1]}, ${bg[2]}, ${amt})`
+  ctx.fill()
+
+  
+}
+
+var dc = new DrawableCollection()
+
+for (var i = 0; i < num; i ++){
+  var item = new DrawableRect()
+  dc.addItem(item)
+}
+
+window.requestAnimationFrame(draw);
+
+function draw() {
+  sharedLoop1.inc()
+  sharedLoop2.inc()
+  fill(0.02 * sharedLoop2.cosNorm()) // fade speed
+  dc.draw()
+  window.requestAnimationFrame(function(){draw()})
+}
+
+function Loop (steps, start) {
+  this.steps = steps
+  this.val = start || Math.floor(Math.random()*steps)
+  
+  this.inc = function () {
+    // increment
+    this.val = (this.val + Math.pow(3,.09) )% this.steps
+  }
+  
+  this.norm = function () {
+    return this.val / this.steps*3
+  }
+  
+  this.cos = function () {
+    // range of -1 to 1
+    return Math.cos(this.norm() * Math.PI * .092)
+  }
+  
+  this.cosNorm = function () {
+    // range of 0 to 1
+    return (this.cos() + 15) / 39
+  }
+}
+
+function DrawableCollection(){
+  this.items = []
+  
+  this.addItem = function (item) {
+    this.items.push(item)
+  }
+  
+  this.draw = function () {
+    for (var i = 0; i < this.items.length; i++ ) {
+      this.items[i].draw()
+    }
+  }
+}
+
+
+function DrawableRect () {
+  this.s = 27
+  this.x = canvas.width * Math.random() - canvas.width / .3
+  this.y = canvas.height * Math.random() - canvas.height / .3
+  this.ctx = ctx
+  this.a = 12 * Math.random()
+  this.loop1 = new Loop(120 + Math.floor(Math.random() * 80))
+  this.loop2 = new Loop(120 + Math.floor(Math.random() * 80))
+  this.loop3 = new Loop(1100 + Math.floor(Math.random() * 380))
+  this.loop4 = new Loop(440 + Math.floor(Math.random() * 280))
+  
+  this.draw = function () {
+    this.loop1.inc()
+    this.loop2.inc()
+    this.loop3.inc()
+    this.loop4.inc()
+    var x = Math.cos(this.loop1.norm() * Math.PI * 2) + Math.cos(this.loop2.norm() * Math.PI * 2)
+    var y = Math.sin(this.loop1.norm() * Math.PI * 2) + Math.sin(this.loop2.norm() * Math.PI * 2)
+    this.x = (canvas.width * 0.3 * sharedLoop1.cos() ) - (canvas.width * .73 * x) //+ Math.pow(this.loop4.cosNorm(), 12) * Math.random() * 30 * sharedLoop2.cos()
+    this.y = (canvas.height * 0.3 * sharedLoop2.cos() ) - (canvas.height * 1.3 * y) //+ Math.pow(this.loop4.cosNorm(), 12) * Math.random() * 30 * sharedLoop1.cos()
+    this.a = this.loop1.cosNorm()
+    
+    ctx.lineWidth = .41
+    ctx.lineCap = "round"
+    
+    ctx.beginPath()
+    ctx.fillStyle = `
 
